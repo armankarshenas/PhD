338ec9e81cbde87a315af7e02fdd5bbaf5937da3
Arman Karshenas <karshenas@Armans-MacBook-Pro.local>
2022-06-10 14:24:42 -0700

    Adding the first scripts

---
+++ Reg-seq/SequenceDataProcessing.m
@@ -0,0 +1,4 @@
+%% Sequence-expression data processing 
+% This script reads in CSV file from Rob Philips' group at Caltech and
+% reshapes sequences to images and exports them into a new directory along
+% with the corresponding expression level
