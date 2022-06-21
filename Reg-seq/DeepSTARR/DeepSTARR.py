8e5164f294533f6123d3a9c5a7ba9558b810a79e
Arman Karshenas <karshenas@Armans-MacBook-Pro.local>
2022-06-21 10:07:53 -0700

    Updated deepstarr training scripts

--- Reg-seq/DeepSTARR/DeepSTARR.py
+++ Reg-seq/DeepSTARR/DeepSTARR.py
@@ -1,5 +1,6 @@
-import tensorflow as tf
 
+print("Importing packages .../n")
+import tensorflow as tf
 import keras
 import keras.layers as kl
 from keras.layers.convolutional import Conv1D, MaxPooling1D
@@ -19,3 +20,178 @@ from helper import IOHelper, SequenceHelper # from https://github.com/const-ae/N
 
 import random
 random.seed(1234)
+
+print("Downloading fasta files .../n ")
+# FASTA files with DNA sequences of genomic regions from train/val/test sets
+#!wget 'https://data.starklab.org/almeida/DeepSTARR/Data/Sequences_activity_Train.fa'
+#!wget 'https://data.starklab.org/almeida/DeepSTARR/Data/Sequences_activity_Val.fa'
+#!wget 'https://data.starklab.org/almeida/DeepSTARR/Data/Sequences_activity_Test.fa'
+
+# Files with developmental and housekeeping activity of genomic regions from train/val/test sets
+#!wget 'https://data.starklab.org/almeida/DeepSTARR/Data/Sequences_activity_Train.txt'
+#!wget 'https://data.starklab.org/almeida/DeepSTARR/Data/Sequences_activity_Val.txt'
+#!wget 'https://data.starklab.org/almeida/DeepSTARR/Data/Sequences_activity_Test.txt'
+
+print("Reading in the inputs ")
+# Data for train/val/test sets
+X_train_sequence, X_train_seq_matrix, X_train, Y_train = prepare_input("Train")
+X_valid_sequence, X_valid_seq_matrix, X_valid, Y_valid = prepare_input("Valid")
+X_test_sequence, X_test_seq_matrix, X_test, Y_test = prepare_input("Test")
+
+print("Setting training parameters")
+
+params = {'batch_size': 128,
+          'epochs': 100,
+          'early_stop': 10,
+          'kernel_size1': 7,
+          'kernel_size2': 3,
+          'kernel_size3': 5,
+          'kernel_size4': 3,
+          'lr': 0.002,
+          'num_filters': 256,
+          'num_filters2': 60,
+          'num_filters3': 60,
+          'num_filters4': 120,
+          'n_conv_layer': 4,
+          'n_add_layer': 2,
+          'dropout_prob': 0.4,
+          'dense_neurons1': 256,
+          'dense_neurons2': 256,
+          'pad':'same'}
+
+
+print("Training the model .../n")
+main_model, main_params = DeepSTARR()
+main_model, my_history = train(main_model, X_train, Y_train, X_valid, Y_valid, main_params)
+
+
+print("Evaluating the model ... /n")
+# run for each set and enhancer type
+summary_statistics(X_train, Y_train[0], "train", "ct_DNA")
+summary_statistics(X_train, Y_train[1], "train", "ct_RNA")
+summary_statistics(X_valid, Y_valid[0], "validation", "ct_DNA")
+summary_statistics(X_valid, Y_valid[1], "validation", "ct_RNA")
+summary_statistics(X_test, Y_test[0], "test", "ct_DNA")
+summary_statistics(X_test, Y_test[1], "test", "ct_RNA")
+
+print("Saving the trained model .../n")
+model_name="Ecoli_regseq"
+
+model_json = main_model.to_json()
+with open('Model_' + model_name + '.json', "w") as json_file:
+    json_file.write(model_json)
+main_model.save_weights('Model_' + model_name + '.h5')
+
+
+
+# function to load sequences and enhancer activity
+def prepare_input(set):
+    # Convert sequences to one-hot encoding matrix
+    file_seq = str(set+"_sequences.fa")
+    input_fasta_data_A = IOHelper.get_fastas_from_file(file_seq, uppercase=True)
+
+    # get length of first sequence
+    sequence_length = len(input_fasta_data_A.sequence.iloc[0])
+
+    # Convert sequence to one hot encoding matrix
+    seq_matrix_A = SequenceHelper.do_one_hot_encoding(input_fasta_data_A.sequence, sequence_length,
+                                                      SequenceHelper.parse_alpha_to_seq)
+    print(seq_matrix_A.shape)
+
+    X = np.nan_to_num(seq_matrix_A) # Replace NaN with zero and infinity with large finite numbers
+    X_reshaped = X.reshape((X.shape[0], X.shape[1], X.shape[2]))
+
+    Activity = pd.read_table(set +"_activity.txt")
+    ct_RNA = Activity.ct_RNA
+    ct_DNA = Activity.ct_DNA
+    Y = [ct_RNA, ct_DNA]
+
+    print(set)
+
+    return input_fasta_data_A.sequence, seq_matrix_A, X_reshaped, Y
+
+### Additional metrics
+from scipy.stats import spearmanr
+def Spearman(y_true, y_pred):
+     return ( tf.py_function(spearmanr, [tf.cast(y_pred, tf.float32),
+                       tf.cast(y_true, tf.float32)], Tout = tf.float32) )
+
+
+def DeepSTARR(params=params):
+
+    lr = params['lr']
+    dropout_prob = params['dropout_prob']
+    n_conv_layer = params['n_conv_layer']
+    n_add_layer = params['n_add_layer']
+
+    # body
+    input = kl.Input(shape=(160, 4))
+    x = kl.Conv1D(params['num_filters'], kernel_size=params['kernel_size1'],
+                  padding=params['pad'],
+                  name='Conv1D_1st')(input)
+    x = BatchNormalization()(x)
+    x = Activation('relu')(x)
+    x = MaxPooling1D(2)(x)
+
+    for i in range(1, n_conv_layer):
+        x = kl.Conv1D(params['num_filters'+str(i+1)],
+                      kernel_size=params['kernel_size'+str(i+1)],
+                      padding=params['pad'],
+                      name=str('Conv1D_'+str(i+1)))(x)
+        x = BatchNormalization()(x)
+        x = Activation('relu')(x)
+        x = MaxPooling1D(2)(x)
+
+    x = Flatten()(x)
+
+    # dense layers
+    for i in range(0, n_add_layer):
+        x = kl.Dense(params['dense_neurons'+str(i+1)],
+                     name=str('Dense_'+str(i+1)))(x)
+        x = BatchNormalization()(x)
+        x = Activation('relu')(x)
+        x = Dropout(dropout_prob)(x)
+    bottleneck = x
+
+    # heads per task (developmental and housekeeping enhancer activities)
+    tasks = ['ct_DNA', 'ct_RNA']
+    outputs = []
+    for task in tasks:
+        outputs.append(kl.Dense(1, activation='linear', name=str('Dense_' + task))(bottleneck))
+
+    model = keras.models.Model([input], outputs)
+    model.compile(keras.optimizers.Adam(lr=lr),
+                  loss=['mse', 'mse'], # loss
+                  loss_weights=[1, 1], # loss weigths to balance
+                  metrics=[Spearman]) # additional track metric
+
+    return model, params
+
+
+DeepSTARR()[0].summary()
+DeepSTARR()[1] # dictionary
+
+
+def train(selected_model, X_train, Y_train, X_valid, Y_valid, params):
+
+    my_history=selected_model.fit(X_train, Y_train,
+                                  validation_data=(X_valid, Y_valid),
+                                  batch_size=params['batch_size'], epochs=params['epochs'],
+                                  callbacks=[EarlyStopping(patience=params['early_stop'], monitor="val_loss", restore_best_weights=True),
+                                             History()])
+
+    return selected_model, my_history
+
+from scipy import stats
+from sklearn.metrics import mean_squared_error
+
+# create functions
+def summary_statistics(X, Y, set, task):
+    pred = main_model.predict(X, batch_size=main_params['batch_size'])
+    if task =="Dev":
+        i=0
+    if task =="Hk":
+        i=1
+    print(set + ' MSE ' + task + ' = ' + str("{0:0.2f}".format(mean_squared_error(Y, pred[i].squeeze()))))
+    print(set + ' PCC ' + task + ' = ' + str("{0:0.2f}".format(stats.pearsonr(Y, pred[i].squeeze())[0])))
+    print(set + ' SCC ' + task + ' = ' + str("{0:0.2f}".format(stats.spearmanr(Y, pred[i].squeeze())[0])))
--- Reg-seq/SequenceDataProcessing.m
+++ Reg-seq/SequenceDataProcessing.m
@@ -4,9 +4,12 @@
 % with the corresponding expression level
  
 %% Specifications 
-Path_to_data = "~/Arman/BerkeleyPhD/Yr2/Reg-seq";
+Path_to_data = "~/Arman/BerkeleyPhD/Yr2/Reg-seq/DawData";
+Path_to_save = "~/Arman/BerkeleyPhD/Yr2/Reg-seq/Data";
 % bit assignment will be done based on A:1, C:2, G:3, T:4
-
+train_f = 0.7;
+test_f = 0.15;
+% training and test fraction of the data 
 
 
 %% Main code body
@@ -30,6 +33,48 @@ fprintf("Showing a preview of the binary sequence ...\n");
 head(binary_seq)
 cd(Path_to_data)
 writetable(binary_seq,"Binary_seq.csv");
+%% Write fasta files 
+header = string(sequences.gene(:)) + "_"+cell2mat(sequences.barcode(1:height(sequences)));
+sequences{:,width(sequences)+1} = header;
+sequences.Properties.VariableNames{'Var7'} = 'Header';
+index = randperm(size(sequences,1));
+sequences = sequences(index,:);
+train_idx = floor(size(sequences,1)*train_f);
+test_idx = floor(size(sequences,1)*test_f);
+val_idx = size(sequences,1) - train_idx-test_idx;
+cd(Path_to_save)
+% Training data
+fastawrite("Train_sequences.fa",string(sequences.Header(1:train_idx)),string(sequences.sequence(1:train_idx)));
+tb_train = table();
+tb_train(:,1) = table(sequences.Header(1:train_idx));
+tb_train(:,2) = table(sequences.ct_DNA(1:train_idx));
+tb_train(:,3) = table(sequences.ct_RNA(1:train_idx));
+tb_train.Properties.VariableNames{'Var1'} = 'Header';
+tb_train.Properties.VariableNames{'Var2'} = 'ct_DNA';
+tb_train.Properties.VariableNames{'Var3'} = 'ct_RNA';
+writetable(tb_train,"Train_activity.txt");
+% Testing data 
+fastawrite("Test_sequences.fa",string(sequences.Header(train_idx+1:train_idx+test_idx)),string(sequences.sequence(train_idx+1:train_idx+test_idx)));
+tb_test = table();
+tb_test(:,1) = table(sequences.Header(train_idx+1:train_idx+test_idx));
+tb_test(:,2) = table(sequences.ct_DNA(train_idx+1:train_idx+test_idx));
+tb_test(:,3) = table(sequences.ct_RNA(train_idx+1:train_idx+test_idx));
+tb_test.Properties.VariableNames{'Var1'} = 'Header';
+tb_test.Properties.VariableNames{'Var2'} = 'ct_DNA';
+tb_test.Properties.VariableNames{'Var3'} = 'ct_RNA';
+writetable(tb_test,"Test_activity.txt");
+% Validation data
+fastawrite("Valid_sequences.fa",string(sequences.Header(train_idx+test_idx+1:end)),string(sequences.sequence(train_idx+test_idx+1:end)));
+tb_val = table();
+tb_val(:,1) = table(sequences.Header(train_idx+test_idx+1:end));
+tb_val(:,2) = table(sequences.ct_DNA(train_idx+test_idx+1:end));
+tb_val(:,3) = table(sequences.ct_RNA(train_idx+test_idx+1:end));
+tb_val.Properties.VariableNames{'Var1'} = 'Header';
+tb_val.Properties.VariableNames{'Var2'} = 'ct_DNA';
+tb_val.Properties.VariableNames{'Var3'} = 'ct_RNA';
+writetable(tb_test,"Valid_activity.txt");
+
+
 %% Image generation 
 fprintf("Writing images ... \n")
 cd(Path_to_data)
