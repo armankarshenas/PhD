2fd4e87a8b9615ab8f0ddeb3b1d3bbdfe82f2541
Arman Karshenas <karshenas@Armans-MacBook-Pro.local>
2022-07-25 13:13:47 -0700

    Changed the implementation of sequence processing - now we first filter the sequcnes, label them and then parition them into three subsets for training, validation and testing.

---
+++ Reg-seq/Matlab/Scripts/FindThreshold.m
@@ -0,0 +1,8 @@
+function [th_up,th_down] = FindThreshold(tb_gene,tb_col,act_thresh)
+    tb_col_name = string(tb_gene.Properties.VariableNames);
+    tb_col_name = tb_col_name == tb_col;
+    vec_values = table2array(tb_gene(:,tb_col_name));
+    vec_sorted = sort(vec_values);
+    th_down = vec_sorted(floor(act_thresh*length(vec_sorted)));
+    th_up = vec_sorted(floor((1-act_thresh)*length(vec_sorted)));
+end
---
+++ Reg-seq/Matlab/Scripts/PartitionDataSet.m
@@ -0,0 +1,9 @@
+function [tb_test,tb_train,tb_valid] = PartitionDataSet(frac_train,frac_test,dataset)
+index = randperm(size(dataset,1));
+dataset = dataset(index,:);
+train_idx = floor(size(dataset,1)*frac_train);
+test_idx = floor(size(dataset,1)*frac_test);
+tb_train = dataset(1:train_idx,:);
+tb_test = dataset(train_idx+1:train_idx+test_idx,:);
+tb_valid = dataset(train_idx+test_idx+1:end,:);
+end
