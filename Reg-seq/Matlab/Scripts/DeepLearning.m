0b3ad0957468c43b8c028f21025bd7aafc19b220
Arman Karshenas <karshenas@Armans-MacBook-Pro.local>
2022-07-27 15:08:50 -0700

    DeepLearning script that calls all the functions to load, train and evaluate the networks

---
+++ Reg-seq/Matlab/Scripts/DeepLearning.m
@@ -0,0 +1,11 @@
+%% Info
+% This script 1: loads the dataset, 2: sets the network architechure, 3:
+% train the network, 4: evaluate the model, and 5: plots any neccessary
+% figures
+
+%% Specifications 
+
+addpath(genpath("/media/zebrafish/Data2/Arman/PhD/Reg-seq/Matlab/Scripts"))
+Path_to_data = "/media/zebrafish/Data2/Arman/Data/LB_dataset";
+Path_to_save = "/media/zebrafish/Data2/Arman/Data/LB_dataset";
+Path_to_save_imgs = "/media/zebrafish/Data2/Arman/Data/LB_dataset/imgs";
