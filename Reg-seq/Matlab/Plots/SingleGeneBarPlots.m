c224b1214de04e48a089c42df1824549f99161cd
Arman Karshenas <karshenas@Armans-MacBook-Pro.local>
2022-08-08 11:14:47 -0700

    New plot for datapoint vs gene

--- Reg-seq/Matlab/Plots/SingleGeneBarPlots.m
+++ Reg-seq/Matlab/Plots/SingleGeneBarPlots.m
@@ -1,6 +1,7 @@
 function [] = SingleGeneBarPlots(ACC)
 %% Plotting bar charts for single gene activity accuracy 
 fig1 = figure();
+subplot(2,1,1);
 hold on;
 tb = struct2table(ACC);
 acc = (tb.acc);
@@ -20,5 +21,10 @@ xtips1 = b(1).XEndPoints;
 ytips1 = b(1).YEndPoints;
 labels1 = sorted_label;
 text(xtips1,ytips1,labels1,'HorizontalAlignment','center','VerticalAlignment','bottom')
+hold on
+subplot(2,1,2);
+scatter(sorted_name,sorted_label,'*','r');
+xlabel("Genes",'FontSize',15,'Interpreter','latex');
+ylabel("Number of data points",'FontSize',15,'Interpreter','latex');
 saveas(fig1,"ACCBarChart.png");
 end
