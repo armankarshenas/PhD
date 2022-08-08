1a8ca5ae9bf6a398cbce1c74b63d172afea628ec
bmoretti <morettibruno@gmail.com>
2022-08-08 12:10:55 -0700

    Data point plot

--- Reg-seq/Matlab/Plots/SingleGeneBarPlots.m
+++ Reg-seq/Matlab/Plots/SingleGeneBarPlots.m
@@ -1,4 +1,4 @@
-function [] = SingleGeneBarPlots(ACC)
+function [] = SingleGeneBarPlots(ACC,Path_to_save)
 %% Plotting bar charts for single gene activity accuracy 
 fig1 = figure();
 subplot(2,1,1);
@@ -6,25 +6,25 @@ hold on;
 tb = struct2table(ACC);
 acc = (tb.acc);
 name = string(tb.gene);
-label = string(tb.datapt);
+label = tb.datapt;
 [sorted_acc,indx] = sort(acc);
+[sorted_dtp,indxdtp] = sort(label);
 sorted_name = [];
 sorted_label =[];
 for i=1:length(acc)
     sorted_name = [sorted_name name(indx(i))];
-    sorted_label = [sorted_label label(indx(i))];
+    sorted_label = [sorted_label name(indxdtp(i))];
 end
 sorted_name = categorical(sorted_name);
 sorted_name = reordercats(sorted_name,indx);
 b = bar(sorted_name,sorted_acc,'FaceColor','red','FaceAlpha',0.4,'EdgeColor','black');
-xtips1 = b(1).XEndPoints;
-ytips1 = b(1).YEndPoints;
-labels1 = sorted_label;
-text(xtips1,ytips1,labels1,'HorizontalAlignment','center','VerticalAlignment','bottom')
 hold on
 subplot(2,1,2);
-scatter(sorted_name,sorted_label,'*','r');
+sorted_label = categorical(sorted_label);
+sorted_label = reordercats(sorted_label,indxdtp);
+scatter(sorted_label,sorted_dtp,'*','r');
 xlabel("Genes",'FontSize',15,'Interpreter','latex');
 ylabel("Number of data points",'FontSize',15,'Interpreter','latex');
+cd(Path_to_save)
 saveas(fig1,"ACCBarChart.png");
 end
