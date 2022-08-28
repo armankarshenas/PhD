8a3154df8c110c574ee61e1745994b0b3d6b77b2
bmoretti <morettibruno@gmail.com>
2022-08-28 15:34:11 -0700

    Finished implementing salientMap scripts using gradCAM function

--- Reg-seq/Matlab/Plots/PlotSalientMap.m
+++ Reg-seq/Matlab/Plots/PlotSalientMap.m
@@ -1,8 +1,10 @@
 function [] = PlotSalientMap(Image,cmap,seq,Path_to_save,name)
+    seq = split(string(seq),"");
+    seq = seq(2:end-1);
     cd(Path_to_save);
     name = name +".fig";
     figure()
-    heatmap(Image,'Colormap',cmap,'XDisplayLabels',seq,'YDisplayLabels',["A","T","C","G"]);
+    heatmap(Image,'Colormap',cmap,'XDisplayLabels',(seq),'YDisplayLabels',["A","T","C","G"]);
     saveas(gcf,name);
     close
 end
